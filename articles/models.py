895b4d365020782416c150a3fb3a355a9ed7139a
Courage Abudu <courageabudu@gmail.com>
2022-03-05 05:13:38 +0100

    Still on the outlook

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from pyexpat import model
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.db import models
@@ -12,10 +13,20 @@ class Article(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
     
-    
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse("article_detail", args=[str(self.id)])
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                related_name='comments')
+    comment = models.CharField(max_length=40)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
+    
+    def __str__(self):
+        return self.comment
+    
+    def get_absolute_url(self):
+        return reverse("article_list")
     
